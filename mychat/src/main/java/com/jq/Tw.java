579de7e75052f0842b5a4b2b37ab686d5a06efa6
NoWaterFish <819657451@qq.com>
2019-12-24 21:01:34 +0800

    init  directory

---
+++ mychat/src/main/java/com/jq/Tw.java
@@ -0,0 +1,12 @@
+package com.jq;
+
+/**
+ * @author Jiangqing
+ * @version 1.0
+ * @since 2019/12/24 20:59
+ */
+public class Tw {
+    public static void main(String[] args) {
+        System.out.println("sss");
+    }
+}
---
+++ mychat/src/test/java/T1.java
@@ -0,0 +1,7 @@
+/**
+ * @author Jiangqing
+ * @version 1.0
+ * @since 2019/12/24 20:59
+ */
+public class T1 {
+}
